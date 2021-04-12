a7a118350c8bb43843adfc90e8b73834ec6cc71c
Evgeniy <icancan1414@gmail.com>
2021-04-12 17:22:56 +0300

    Исправил ошибки от код ревью, нужно пройти автоматические тесты

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Union, List, Dict
+from typing import Union, List, Dict, Tuple
 import datetime as dt
 
 
@@ -17,34 +17,30 @@ class Calculator:
 
     def get_today_stats(self) -> float:
         '''Возвращает количество за сегодня.'''
-        self.count_today: float = sum([i.amount for i in self.records
-                                       if i.date == dt.datetime.now().date()])
-        return self.count_today
+        count_today: float = sum([i.amount for i in self.records
+                                  if i.date == dt.datetime.now().date()])
+        return count_today
 
     def get_week_stats(self) -> float:
         '''Возвращает количество за неделю.'''
-        self.count_week: float = 0
-        # количество дней недели
         week: dt.timedelta = dt.timedelta(days=7)
-        for i in self.records:
-            if (dt.datetime.now().date() >= i.date
-               >= (dt.datetime.now().date() - week)):
-                self.count_week += i.amount
-        return self.count_week
+        count_week: float = sum([i.amount for i in self.records
+                                if (dt.datetime.now().date() >= i.date
+                                 >= (dt.datetime.now().date() - week))])
+        return count_week
 
 
 class Record:
     '''Класс для записи данных.'''
 
-    def __init__(self, amount: float, comment: str, date: Union[str,
-                 dt.datetime] = dt.datetime.now()):
+    def __init__(self, amount: float, comment: str,
+                 date: Union[str, None] = None):
+        date_format: str = '%d.%m.%Y'
+        date_priv = (dt.datetime.now().date() if date is None else
+                     dt.datetime.strptime(date, date_format).date())
         self.amount = amount
         self.comment = comment
-        date_format: str = '%d.%m.%Y'
-        if type(date) == dt.datetime:
-            self.date = date.date()
-        else:
-            self.date = dt.datetime.strptime(date, date_format).date()
+        self.date = date_priv
 
 
 class CaloriesCalculator(Calculator):
@@ -69,56 +65,38 @@ class CaloriesCalculator(Calculator):
 
 class CashCalculator(Calculator):
     '''Подсчитывает кол-во денег.'''
+    RUB_RATE: float = 1.0
     USD_RATE: float = 77.0
     EURO_RATE: float = 91.7
+    # в словаре храним валюты, курс и способ вывода
+    currency_dict: Dict[str, Tuple[float, str]]
+    currency_dict = {'rub': (RUB_RATE, 'руб'),
+                     'usd': (USD_RATE, 'USD'),
+                     'eur': (EURO_RATE, 'Euro')}
 
     def get_today_cash_remained(self, currency: str):
         '''Определяет, сколько еще можно потратить.'''
-        self.currency = currency.lower()
-        # принимаемые валюты в виде списка и курсы
-        self.cur: List = ['rub', 'usd', 'eur']
-        # список для вывода валюты
-        self.cur_dict: Dict = {'rub': 'руб', 'usd': 'USD', 'eur': 'Euro'}
+        currency_priv = currency.lower()
+        # распаковываем курс и способ вывода
+        cur_rate, cur_str = self.currency_dict[currency_priv]
         # если валюты нет в списке возвращаем None
-        if self.currency not in self.cur:
+        if currency_priv not in self.currency_dict.keys():
             return None
         total_value: float = self.get_today_stats()
         diff_value: float = 0
-        out_str: str = ''
         # определяем оставшееся количество
         # в зависимости от валюты
-        if self.currency == self.cur[0]:
-            diff_value = self.limit - total_value
-        elif self.currency == self.cur[1]:
-            diff_value = round(
-                (self.limit - total_value) / self.USD_RATE, 2)
-        elif self.currency == self.cur[2]:
-            diff_value = round(
-                (self.limit - total_value) / self.EURO_RATE, 2)
+        diff_value = round(
+            (self.limit - total_value) / cur_rate, 2)
         # выводим запись о количестве
-        if diff_value > 0:
-            out_str = (f'На сегодня осталось '
-                            f'{diff_value:.2f} '
-                            f'{self.cur_dict[self.currency]}')
-        elif diff_value == 0:
+        if diff_value == 0:
             out_str = 'Денег нет, держись'
+        elif diff_value > 0:
+            out_str = (f'На сегодня осталось '
+                       f'{diff_value:.2f} '
+                       f'{cur_str}')
         elif diff_value < 0:
             out_str = (f'Денег нет, держись: твой долг - '
-                            f'{abs(diff_value):.2f} '
-                            f'{self.cur_dict[self.currency]}')
+                       f'{abs(diff_value):.2f} '
+                       f'{cur_str}')
         return out_str
-
-
-kal = CaloriesCalculator(1000)
-kal.add_record(Record(500, 'sss', '12.04.2021'))
-kal.add_record(Record(200, 'sss', '12.04.2021'))
-kal.add_record(Record(100, 'sss', '12.04.2021'))
-
-print(kal.get_calories_remained())
-
-
-cash = CashCalculator(2000)
-cash.add_record(Record(300, 'fdd', '12.04.2021'))
-cash.add_record(Record(300, 'fdd', '12.04.2021'))
-cash.add_record(Record(300, 'fdd', '12.04.2021'))
-print(cash.get_today_cash_remained('rub'))
