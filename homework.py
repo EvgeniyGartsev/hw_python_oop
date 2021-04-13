66c747d8f40a42647d2f5efdb58b0d899c9bd2ae
Evgeniy <icancan1414@gmail.com>
2021-04-13 21:04:47 +0300

    Вывел словарь в тело класса, причесал код

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Union, List, Dict, Tuple
+from typing import Union, List, Dict, Tuple, Optional
 import datetime as dt
 
 
@@ -21,7 +21,7 @@ class Calculator:
                                  if i.date == dt.datetime.now().date())
         return count_today
 
-    def get_diff_today(self):
+    def get_diff_today(self) -> float:
         '''Возвращает оставшееся количество за сегодня'''
         total_value: float = self.get_today_stats()
         diff_value: float = 0
@@ -42,7 +42,7 @@ class Record:
     '''Класс для записи данных.'''
 
     def __init__(self, amount: float, comment: str,
-                 date: Union[str, None] = None):
+                 date: Union[str, None] = None) -> None:
         date_format: str = '%d.%m.%Y'
         date_priv = (dt.datetime.now().date() if date is None else
                      dt.datetime.strptime(date, date_format).date())
@@ -54,52 +54,48 @@ class Record:
 class CaloriesCalculator(Calculator):
     '''Класс подсчета каллорий.'''
 
-    def get_calories_remained(self) -> float:
+    def get_calories_remained(self) -> str:
         '''Определяет, сколько еще можно съесть.'''
         diff_value = self.get_diff_today()
-        out_value: str = ''
         # определяем оставшееся количество
         if diff_value > 0:
-            out_value = (f'Сегодня можно съесть что-нибудь ещё, '
-                         f'но с общей калорийностью не более '
-                         f'{diff_value} кКал')
+            return (f'Сегодня можно съесть что-нибудь ещё, '
+                    f'но с общей калорийностью не более '
+                    f'{diff_value} кКал')
         else:
-            out_value = 'Хватит есть!'
-        return out_value
+            return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
     '''Подсчитывает кол-во денег.'''
     RUB_RATE: float = 1.0
-    USD_RATE: float = 77.0
-    EURO_RATE: float = 91.7
+    USD_RATE: float = 60.0
+    EURO_RATE: float = 70.0
+    # в словаре храним валюты, курс и способ вывода
+    cur_out: Dict[str, Tuple[float, str]]
+    cur_out = {'rub': (RUB_RATE, 'руб'),
+               'usd': (USD_RATE, 'USD'),
+               'eur': (EURO_RATE, 'Euro')}
 
-    def get_today_cash_remained(self, currency: str):
+    def get_today_cash_remained(self, currency: str) -> Optional[str]:
         '''Определяет, сколько еще можно потратить.'''
-        # в словаре храним валюты, курс и способ вывода
-        cur_out: Dict[str, Tuple[float, str]]
-        cur_out = {'rub': (self.RUB_RATE, 'руб'),
-                   'usd': (self.USD_RATE, 'USD'),
-                   'eur': (self.EURO_RATE, 'Euro')}
         currency_priv = currency.lower()
         # если валюты нет в списке возвращаем None
-        if currency_priv not in cur_out.keys():
+        if currency_priv not in self.cur_out.keys():
             return None
         # распаковываем курс и способ вывода
-        cur_rate, cur_str = cur_out[currency_priv]
+        cur_rate, cur_name = self.cur_out[currency_priv]
         # определяем оставшееся количество
         # в зависимости от валюты
         diff_value = round(
             self.get_diff_today() / cur_rate, 2)
         # выводим запись о количестве
         if diff_value == 0:
-            out_value = 'Денег нет, держись'
-        elif diff_value > 0:
-            out_value = (f'На сегодня осталось '
-                         f'{diff_value:.2f} '
-                         f'{cur_str}')
-        elif diff_value < 0:
-            out_value = (f'Денег нет, держись: твой долг - '
-                         f'{abs(diff_value):.2f} '
-                         f'{cur_str}')
-        return out_value
+            return 'Денег нет, держись'
+        if diff_value > 0:
+            return (f'На сегодня осталось '
+                    f'{diff_value:.2f} '
+                    f'{cur_name}')
+        return (f'Денег нет, держись: твой долг - '
+                f'{abs(diff_value):.2f} '
+                f'{cur_name}')
